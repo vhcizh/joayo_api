3a99d39ef7aa620780d2288106e648426a91c375
sso0 <7bsso0@gmail.com>
2023-11-27 21:28:16 +0900

    Initial commit

---
+++ src/main/java/api/joayo/JoayoApplication.java
@@ -0,0 +1,13 @@
+package api.joayo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoayoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoayoApplication.class, args);
+	}
+
+}

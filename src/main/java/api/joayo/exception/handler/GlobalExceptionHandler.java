24f140d0af29464caf50c78738f49afd24686ab6
Soyoung Kim <sybz0748@gmail.com>
2023-12-14 22:33:37 +0900

    전역 오류 처리

---
+++ src/main/java/api/joayo/exception/handler/GlobalExceptionHandler.java
@@ -0,0 +1,15 @@
+package api.joayo.exception.handler;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleAllException(Exception e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
+}

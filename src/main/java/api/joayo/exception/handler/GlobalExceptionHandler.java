bd4f78601a992c543482928177dbd7cd4f2b956f
Soyoung Kim <sybz0748@gmail.com>
2023-12-29 01:38:18 +0900

    유효성 검증 추가

--- src/main/java/api/joayo/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/api/joayo/exception/handler/GlobalExceptionHandler.java
@@ -1,8 +1,13 @@
 package api.joayo.exception.handler;
 
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -10,5 +15,17 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleAllException(Exception e) {
         return ResponseEntity.badRequest().body(e.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleJoinException(MethodArgumentNotValidException e) {
+        // 유효성 검사 예외 처리 로직
+        String errorMsg = e.getBindingResult()
+                .getAllErrors()
+                .stream()
+                .map(ObjectError::getDefaultMessage)
+                .collect(Collectors.joining("\n"));
+
+        return ResponseEntity.badRequest().body(errorMsg);
     }
 }
